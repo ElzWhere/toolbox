7fb26dd8a1a22d82322520a2e9f24fcf64b1e096
elzwhere <elzwhere@gmail.com>
2023-08-28 10:58:23 -0700

    Boiler plate CLI using Cobra

---
+++ cmd/net/ping.go
@@ -0,0 +1,68 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package net
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"net/http"
+	"time"
+)
+
+var (
+	urlPath string
+	client  = http.Client{
+		Timeout: time.Second * 2,
+	}
+)
+
+func ping(domain string) (int, error) {
+	url := "http://" + domain
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	resp.Body.Close()
+	return resp.StatusCode, nil
+}
+
+// pingCmd represents the ping command
+var pingCmd = &cobra.Command{
+	Use:   "ping",
+	Short: "This pings a remote URL and returns the response",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if resp, err := ping(urlPath); err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("Response:", resp)
+
+		}
+	},
+}
+
+func init() {
+
+	pingCmd.Flags().StringVarP(&urlPath, "url", "u", "", "URL to ping")
+
+	if err := pingCmd.MarkFlagRequired("url"); err != nil {
+		panic(err)
+	}
+
+	NetCmd.AddCommand(pingCmd)
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// pingCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// pingCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
